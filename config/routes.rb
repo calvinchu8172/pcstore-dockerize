5f2b894f7b312a53199b96f23977c452c5207f12
calvinchu8172 <calvinchu8172@gmail.com>
2015-02-26 15:33:55 +0800

    Fix root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  get 'static_pages/index'
 
   root 'static_pages#index'
   # The priority is based upon order of creation: first created -> highest priority.
