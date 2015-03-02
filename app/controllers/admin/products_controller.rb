868193ec5d25d02aafbfdd634ce4a10fccac746b
calvinchu8172 <calvinchu8172@gmail.com>
2015-03-02 16:12:53 +0800

    finish product update & delete function and fix product index.html.erb file name bug

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -8,13 +8,32 @@ class Admin::ProductsController < Admin::BaseController
     @product = Product.new
   end
 
+  def edit
+    @product = Product.find_by(id: params[:id])
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    if @product.update(product_params)
+      redirect_to admin_products_path, notice: '成功更新商品'
+    else
+      render :edit
+    end 
+  end
+
   def create
     @product = Product.new(product_params)
-      if @product.save
-        redirect_to admin_products_path, notice: '成功新增商品'
-      else
-        render :new
-      end
+    if @product.save
+      redirect_to admin_products_path, notice: '成功新增商品'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.set_delete
+    redirect_to admin_products_path, notice: '商品已刪除'
   end
 
   private
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,18 @@
 class Product < ActiveRecord::Base
+
+  default_scope{ where(is_deleted: false) }
+
+  def set_delete
+    self.is_deleted = true
+    save
+  end
+
+  def status
+    if is_online
+      "上架中"
+    else
+      "未上架"
+    end
+  end
+
 end
