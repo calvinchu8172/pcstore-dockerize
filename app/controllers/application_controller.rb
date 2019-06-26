c0bfde0bcc72501e4c02781794470dc4618bfece
Calvin Chu <calvinchu@ecoworkinc.com>
2019-06-26 16:26:12 +0800

    Redirect to current url after sign in and specific url after sign out.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :danger #for Bootstap flash style
-
+  before_action :store_user_location!, if: :storable_location?
   before_action :set_locale
 
   def set_locale
@@ -15,10 +15,28 @@ class ApplicationController < ActionController::Base
     I18n.locale = session[:locale] || I18n.default_locale
   end
 
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    # stored_location_for(resource_or_scope) || super
+    products_path
+  end
+
   private
 
   def set_cart
     @cart = Cart.build_from_hash(session["cart"])
+  end
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
+  end
+
+  def store_user_location!
+    # :user is the scope we are authenticating
+    store_location_for(:user, request.fullpath)
   end
 
 end
