cac6a77b219e36633f5594fb31be6ecee9418c08
Calvin Chu <calvinchu@ecoworkinc.com>
2019-06-27 15:50:36 +0800

    Modify welcome page template to HAML.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,8 +20,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource_or_scope)
-    # stored_location_for(resource_or_scope) || super
-    products_path
+    stored_location_for(resource_or_scope) || super
   end
 
   private
