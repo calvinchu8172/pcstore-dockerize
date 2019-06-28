6a05029150010fbbac9782e71c62b0de14d181e6
Calvin Chu <calvinchu@ecoworkinc.com>
2019-06-28 15:47:12 +0800

    Add flash message filter to filter Devise gem flash message key.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def flash_filter key
+    case key
+    when 'notice'
+      key = 'info'
+    when 'alert'
+      key = 'warning'
+    else
+      key
+    end
+    key
+  end
+
 end
