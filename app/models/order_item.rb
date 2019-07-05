15a5af8bd6652cf5c3e447bbc91a22c45903204e
Calvin Chu <calvinchu@ecoworkinc.com>
2019-07-05 20:16:05 +0800

    Fix bug if quantity is blank it will cause price calculation error.

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,9 +1,9 @@
 class OrderItem < ActiveRecord::Base
-  validates :quantity, numericality: { greater_than_or_equal_to: 1 }
   belongs_to :order
   belongs_to :product
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 
   def price
-    product.price * quantity
+    quantity.blank?? 0 : product.price * quantity
   end
 end
