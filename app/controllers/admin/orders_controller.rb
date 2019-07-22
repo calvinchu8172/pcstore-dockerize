fad6af8298f5c17c0014df0f894c7471c8fb3256
Calvin Chu <calvinchu@ecoworkinc.com>
2019-07-22 20:16:55 +0800

    Add search in admin order index page.

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,7 +2,11 @@ class Admin::OrdersController < Admin::BaseController
   before_action :all_categroies
 
   def index
-    @orders = Order.page(params[:page]).per(20)
+    @q = Order.ransack(params[:q])
+    # @data = @q.result(distinct: true).includes(:user, :order_items)
+    @data = @q.result.includes(:user, :order_items)
+    @orders = @data.page(params[:page]).per(20)
+    # @orders = Order.page(params[:page]).per(20)
   end
 
   private
